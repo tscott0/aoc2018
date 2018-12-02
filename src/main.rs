9e70a964efa0a5ee3e20759f60c599e04c895c53
Tom Scott <tomscott87@gmail.com>
2018-12-02 20:01:54 +0000

    Day 2, part 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day_1_part_1;
 mod day_1_part_2;
 mod day_2_part_1;
+mod day_2_part_2;
 mod utils;
 
 fn main() {
@@ -11,6 +12,11 @@ fn main() {
 
     println!(
         "DAY 2 part 1: {}",
-        day_2_part_1::solve(utils::string_from_file("src/day_2_part_1_input.txt"))
+        day_2_part_1::solve(utils::string_from_file("src/day_2_input.txt"))
+    );
+
+    println!(
+        "DAY 2 part 2: {}",
+        day_2_part_2::solve(utils::string_from_file("src/day_2_input.txt"))
     );
 }
