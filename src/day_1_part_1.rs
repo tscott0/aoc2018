317586749fe69416edd7d91eb739ef4a0282f64a
Tom Scott <tomscott87@gmail.com>
2018-12-02 20:08:27 +0000

    Refactor to read from file for day 1

--- src/day_1_part_1.rs
+++ src/day_1_part_1.rs
@@ -1,4 +1,4 @@
-pub fn solve(input: &str) -> i32 {
+pub fn solve(input: String) -> i32 {
     let split = input.split("\n");
     let mut sum: i32 = 0;
 
@@ -12,15 +12,15 @@ pub fn solve(input: &str) -> i32 {
 
 #[test]
 fn example_1() {
-    assert_eq!(3, solve("+1\n+1\n+1"));
+    assert_eq!(3, solve(String::from("+1\n+1\n+1")));
 }
 
 #[test]
 fn example_2() {
-    assert_eq!(0, solve("+1\n+1\n-2"));
+    assert_eq!(0, solve(String::from("+1\n+1\n-2")));
 }
 
 #[test]
 fn example_3() {
-    assert_eq!(-6, solve("-1\n-2\n-3"));
+    assert_eq!(-6, solve(String::from("-1\n-2\n-3")));
 }
--- src/day_1_part_2.rs
+++ src/day_1_part_2.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-pub fn solve(input: &str) -> i32 {
+pub fn solve(input: String) -> i32 {
     let mut frequency_set = HashSet::new();
     let mut sum: i32 = 0;
     frequency_set.insert(sum);
@@ -23,20 +23,20 @@ pub fn solve(input: &str) -> i32 {
 
 #[test]
 fn example_1() {
-    assert_eq!(0, solve("+1\n-1"));
+    assert_eq!(0, solve(String::from("+1\n-1")));
 }
 
 #[test]
 fn example_2() {
-    assert_eq!(10, solve("+3\n+3\n+4\n-2\n-4"));
+    assert_eq!(10, solve(String::from("+3\n+3\n+4\n-2\n-4")));
 }
 
 #[test]
 fn example_3() {
-    assert_eq!(5, solve("-6\n+3\n+8\n+5\n-6"));
+    assert_eq!(5, solve(String::from("-6\n+3\n+8\n+5\n-6")));
 }
 
 #[test]
 fn example_4() {
-    assert_eq!(14, solve("+7\n+7\n-2\n-7\n-4"));
+    assert_eq!(14, solve(String::from("+7\n+7\n-2\n-7\n-4")));
 }
